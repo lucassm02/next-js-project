79d69e9ea5006aec06685fee9fe961407e860147
Lucas Santos <lucassm02@gmail.com>
2020-12-04 22:10:35 -0300

    chore: adds initial configuration to project, eslint, editorconfig and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My App</title>
+      </Head>
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </div>
+  );
+}
