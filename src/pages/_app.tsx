a6686638d7ec7800f2024e7ef4883134a6c65d8c
Lucas Santos <lucassm02@gmail.com>
2020-12-05 12:01:24 -0300

    chore: adds styled components to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { AppProps } from 'next/app';
+import GlobalStyle from '../styles/global';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <GlobalStyle />
+  </>
 );
 
 export default MyApp;
